5812da314b2eb49006974bd1228339c6f41972fd
AdamKelly <adam.obare@student.moringaschool.com>
2024-01-17 23:33:03 +0300

    updated

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,17 @@
+// next image
+import Image from 'next/image';
+
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return <div className='hidden xl:flex xl:max-w-none'>
+    <Image 
+    src={'/pic3.png'} 
+    width={1000}  
+    height={150}
+    alt='' 
+    className='translate-z-0 w-full h-full'
+    />
+  </div>;
 };
 
 export default Avatar;
